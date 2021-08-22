08553256bb6595085e8240f08262be9b4d2d1b45
patryk-harasiuk <patryk.harasiuk@skygate.io>
2021-08-22 10:38:07 +0200

    fh-6-add base code for css

---
+++ frontend/src/config/stylesConfig.ts
@@ -0,0 +1,18 @@
+export const fontWeight = {
+  light: 300,
+  medium: 400,
+  extraMedium: 500,
+  semiBold: 600,
+  heavy: 700,
+  extraBold: 800,
+};
+
+export const fontSize = {
+  xxSmall: '0.7rem',
+  xSmall: '0.8rem',
+  small: '0.85rem',
+  normal: '1rem',
+  medium: '1.2rem',
+  large: '1.5rem',
+  extraLarge: '2rem',
+};
