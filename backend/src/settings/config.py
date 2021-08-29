a00e94ba504eeab532f7eaa26c08dbd3676af1c0
jakub-figat <jakub.figat@skygate.io>
2021-08-29 12:23:01 +0200

    fh-8 Install fastapi-mail, create e-mail config

--- backend/src/settings/config.py
+++ backend/src/settings/config.py
@@ -1,4 +1,5 @@
 from decouple import config
+from fastapi_mail import ConnectionConfig
 
 MONGO_DATABASE = config("MONGODB_INITDB_DATABASE")
 MONGO_USERNAME = config("MONGO_INITDB_ROOT_USERNAME")
@@ -9,4 +10,17 @@ MONGO_PORT = config("MONGO_PORT")
 
 MONGO_URL = (
     f"mongodb://{MONGO_USERNAME}:{MONGO_PASSWORD}@" f"{MONGO_HOST}:{MONGO_PORT}/{MONGO_DATABASE}?authSource=admin"
+)
+
+
+email_config = ConnectionConfig(
+    MAIL_USERNAME=config("EMAIL_USERNAME"),
+    MAIL_PASSWORD=config("EMAIL_PASSWORD"),
+    MAIL_FROM=config("EMAIL_USERNAME"),
+    MAIL_PORT=config("EMAIL_PORT"),
+    MAIL_SERVER=config("EMAIL_HOST"),
+    MAIL_TLS=config("EMAIL_USE_TLS", cast=bool),
+    MAIL_SSL=False,
+    USE_CREDENTIALS=True,
+    TEMPLATE_FOLDER=config("TEMPLATE_DIR"),
 )
